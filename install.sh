b4a659d15ab725a280377fb9e7f7721108fcc845
PMrvic <87659728+PMrvic@users.noreply.github.com>
2021-07-19 18:07:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt-get install mdk3
 sudo apt-get install python3
 sudo apt-get install pip3
 echo ""
-echo "Install is complete, run wifi_scam.py (sudo python3 wifi_scam.py)"
+echo "Install is complete, run wifi_spam.py (sudo python3 wifi_spam.py)"
